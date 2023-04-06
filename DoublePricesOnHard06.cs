d248e673a305c8401dccd74d34855533a2dd9aa3
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-06 11:33:11 +0200

    crap i inverted double and normal prices...

---
+++ DoublePricesOnHard06.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+using double_prices_on_hard_06;
+
+[assembly: MelonInfo(typeof(DoublePricesOnHard06), "Double prices on Hard (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace double_prices_on_hard_06;
+public class DoublePricesOnHard06 : MelonMod
+{
+    [HarmonyPatch(typeof(fclShopCalc), nameof(fclShopCalc.shpCalcItemPrice))]
+    private class Patch
+    {
+        public static void Postfix(ref int __result)
+        {
+            // If on Hard then multiply prices by 1/3
+            if (dds3ConfigMain.cfgGetBit(9u) == 2) __result = __result * 1 / 3;
+        }
+    }
+}
